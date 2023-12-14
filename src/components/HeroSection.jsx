930e3c3de94d5ec7409c7111e2a4bd5dcd5537d7
Syeda Qirrat Fatima <syedaqirratfatima@gmail.com>
2023-12-14 14:59:45 +0500

    commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,81 +1,95 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import React, { useState } from 'react';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import img from "../assets/img/img.png";
 
-const HeroSection = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [mobileNumber, setMobileNumber] = useState('');
-  const [lookingToInvest, setLookingToInvest] = useState('');
-
-  const handleCheckboxChange = () => {
-    // Update this function according to your needs
-    setLookingToInvest(!lookingToInvest);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add your form submission logic here
-  };
-
-  const spanStyle = {
-    color: '#333399',
-  };
-
+export default function HeroSection() {
   return (
-    <div className="hero-section">
-      <h1>
-        <br></br>
-        <b> Request <span style={spanStyle}>Callback</span></b>
-      </h1>
-      <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <div className="form-row">
-            <div className="form-col">
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} required />
-              <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+    <div className="main-container">
+      <Container
+        style={{ paddingTop: "3rem", paddingBottom: "3rem", columnGap: "1rem" }}
+      >
+        <Row>
+          <Col lg={8} sm={12}>
+            <h1 className="heading">
+              Request <span style={{ color: "#4040DA" }}>Callback</span>
+            </h1>
+            <Container>
+              <Row>
+                <Col>
+                  Name <span style={{ color: "#4040DA" }}>*</span>
+                  <br />
+                  <input
+                    placeholder="John"
+                    style={{ paddingLeft: "6px" }}
+                  ></input>
+                </Col>
+                <Col>
+                  Email<span style={{ color: "#4040DA" }}>*</span>
+                  <br />
+                  <input
+                    placeholder="john@email.com"
+                    style={{ paddingLeft: "6px" }}
+                  ></input>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  Mobile no. <span style={{ color: "#4040DA" }}>*</span>
+                  <br />
+                  <input
+                    placeholder="00 (123) 456 78 90"
+                    style={{ paddingLeft: "6px" }}
+                  ></input>
+                </Col>
+                <Col>
+                  Looking to invest (in Rs.)
+                  <br />
+                  <input
+                    placeholder="john@email.com"
+                    style={{ paddingLeft: "6px" }}
+                  ></input>
+                </Col>
+              </Row>
+            </Container>
+            <div>
+              <input
+                type="checkbox"
+                style={{ width: "auto", marginLeft: "14px", display: "inline" }}
+              ></input>
+              <span style={{ display: "inline" }}>
+                I authorize Estee Advisors Pvt. Ltd. to contact me. This will
+                override registry on the NDNC. As per SEBI guide Lines, minimum
+                investment required is of ₹50 Lakhs.
+              </span>
             </div>
-
-            <div className="form-col">
-              <label htmlFor="mobileNumber">Mobile Number:</label>
-              <input type="tel" id="mobileNumber" name="mobileNumber" value={mobileNumber} onChange={(e) => setMobileNumber(e.target.value)} required />
-              <label htmlFor="lookingToInvest">Looking to Invest:</label>
-              {/* Use a dropdown/select for lookingToInvest */}
-              <select
-                id="lookingToInvest"
-                name="lookingToInvest"
-                value={lookingToInvest}
-                onChange={(e) => setLookingToInvest(e.target.value)}
-                required
-              >
-                <option value="">Select an option</option>
-                <option value="yes">Yes</option>
-                <option value="no">No</option>
-              </select>
+            <div className="callback-btn">Request Callback</div>
+          </Col>
+          <Col
+            lg={4}
+            sm={12}
+            style={{
+              backgroundColor: "#A73A3A ",
+              width: "418px",
+              height: "500px",
+              borderRadius: "60px",
+              position: "absolute",
+              left: "39.5rem",
+              top: "6rem",
+            }}
+          >
+            <div>
+              <img
+                src={img}
+                alt="img"
+                style={{
+                  width: "auto",
+                  height: "500px",
+                }}
+              />
             </div>
-          </div>
-
-          <div className="form-checkbox">
-            <input type="checkbox" id="authorizeCheckbox" name="authorizeCheckbox" required />
-            <label htmlFor="authorizeCheckbox">I authorize Estee Advisors Pvt. Ltd. to contact me. This will override registry on the NDNC. As per SEBI guide Lines, minimum investment required is of ₹50 Lakhs.</label>
-          </div>
-
-          <button type="submit" className="form-button">
-            Request Callback
-          </button>
-        </form>
-      </div>
-
-      <div className="red-column">
-        <div className="red-background">
-          <img src={img} alt="Your Image" />
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-};
-
-export default HeroSection;
+}
